08d87b2efa9b38488b160e7e6236039b7b957fdf
filipef9 <filipef9@hotmail.com>
2021-09-13 19:01:42 +0000

    Limpeza de código e assets desnecessários.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>igti-vue-todo</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
